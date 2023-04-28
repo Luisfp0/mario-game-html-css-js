29cf476ded20b21b870ce29e48a69b95f53281a6
Luís Fernando <luisfernandodepaulosousa0020@gmail.com>
2023-04-28 13:40:05 -0300

    Ao triscar no cano, a animação para

--- js/script.js
+++ js/script.js
@@ -6,4 +6,18 @@ const jump = () => {
   }, 700);
 }
 
+const loop = setInterval(() => {
+  const mario = document.querySelector('.mario');
+  const pype = document.querySelector('.pype');
+  let pypePosition = pype.offsetLeft
+  if (pypePosition < 470 && pypePosition > 450) {
+    if (mario.classList.contains('jump')) {
+      return
+    } else {
+      pype.style.animation = 'none';
+      pype.classList.add('pype-left');
+    }
+  }
+}, 50)
+
 document.addEventListener('keydown', jump); a
